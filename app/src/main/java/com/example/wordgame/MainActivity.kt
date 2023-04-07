2d80fc42ed1df7a5e009f0f57003dbdba8753416
tahapek5454 <tahapek5454@gmail.com>
2023-04-07 17:55:36 +0300

    feat: add falling animation

--- app/src/main/java/com/example/wordgame/MainActivity.kt
+++ app/src/main/java/com/example/wordgame/MainActivity.kt
@@ -9,6 +9,8 @@ import android.os.Looper
 import android.widget.Button
 import com.example.wordgame.domain.entities.Character
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,10 +20,6 @@ class MainActivity : AppCompatActivity() {
     var characters = ArrayList<Character>();
     //selectedCharacters
     var selectedCharacterIntex: Int?=null;
-    //starter animation handler
-    var starterAnimationRunnable: Runnable = Runnable{};
-    var starterAnimationHandler = Handler(Looper.myLooper()!!);
-    var flagStarAnimationHandler: Boolean = false;
 
 
 
@@ -72,34 +70,34 @@ class MainActivity : AppCompatActivity() {
 
         //start begin animation for 16 values
         var rowCounter:Int = 8;
-        var a :Int = 1;
-        starterAnimationRunnable = object :Runnable{
-            override fun run() {
+        var starterAnimation  = object :CountDownTimer(10000,200){
+            override fun onTick(p0: Long) {
                 if(rowCounter >= 64){
                     // 64 is starter index of ninth row
-                    flagStarAnimationHandler = true;
-                }
-                starterAnimationHandler.postDelayed(starterAnimationRunnable, 300);
-                setViewsStarterAnimation(rowCounter);
-
-                rowCounter = rowCounter +8;
-
-            }
-        }
-        starterAnimationHandler.post(starterAnimationRunnable);
-
-        val starterAnimationCloseCD = object :CountDownTimer(5000, 200){
-            override fun onTick(p0: Long) {
-                if(flagStarAnimationHandler){
-                    starterAnimationHandler.removeCallbacks(starterAnimationRunnable);
                     cancel()
                 }
+                setViewsStarterAnimation(rowCounter);
+                rowCounter = rowCounter +8;
             }
-            override fun onFinish() {
-
-            }
+            override fun onFinish() {}
         }
-        starterAnimationCloseCD.start()
+        starterAnimation.start()
+        //end
+
+        //start fallAnimation by timer
+        var isFirstFallAnimation = true;
+        object :CountDownTimer(50000000, 6000){
+            override fun onTick(p0: Long) {
+
+                if(isFirstFallAnimation){
+                    // we have just wasted first 4 second time here
+                    isFirstFallAnimation = false;
+                }else{
+                    startRandomFallAnimation()
+                }
+            }
+            override fun onFinish() {}
+        }.start()
         //end
 
         //start add clickLister to all buttons
@@ -119,8 +117,7 @@ class MainActivity : AppCompatActivity() {
         //start apply response clicklistener
         applyButton.setOnClickListener{
             // have to add API function
-            println("Butonla kapadim")
-            starterAnimationHandler.removeCallbacks(starterAnimationRunnable);
+
         }
         //end
 
@@ -156,6 +153,81 @@ class MainActivity : AppCompatActivity() {
     }
     //end
 
+    //start begin random FallAnimation timer
+    fun startRandomFallAnimation(){
+        var fallingColumn = createRandomColumn();
+        var fallIndexCounter = fallingColumn;
+        var createdCharacter = createRandomCharacter();
+
+        var fallingAnimation = object :CountDownTimer(100000, 500){
+            override fun onTick(p0: Long) {
+                if(
+                    fallIndexCounter > 72||
+                    fallIndexCounter > 73||
+                    fallIndexCounter > 74||
+                    fallIndexCounter > 75||
+                    fallIndexCounter > 76||
+                    fallIndexCounter > 77||
+                    fallIndexCounter > 78||
+                    fallIndexCounter > 79
+                ){
+                    // we reach some last row
+                    cancel()
+                }
+                var isDone = setViewRandomFallAnimation(fallIndexCounter, createdCharacter)
+                if(isDone){
+                    // have under value
+                    cancel()
+                }
+                fallIndexCounter = fallIndexCounter + 8;
+            }
+            override fun onFinish() {}
+        }
+        fallingAnimation.start()
+    }
+    //end
+
+    //start set view randomFallAnimation
+    fun setViewRandomFallAnimation(fallIndex:Int, character:String): Boolean{
+        var index = fallIndex;
+
+        //first Row condition
+        if(index <= 7){
+            //first row
+            //speacial situatin not deleteing before
+            var button : Button = buttons[index];
+            // check is game over ?
+            if(button.text.toString().length !=0){
+                // game should be over
+                // for now game doesn't end
+                return true;
+            }else{
+                button.text = character;
+                button.setBackgroundColor(Color.parseColor("#E8A0BF"))
+                return  false;
+            }
+        }
+        //another Row condition
+        else{
+            var oldButton:Button = buttons[index - 8];
+            var nextButton:Button = buttons[index];
+
+            // if nextButton has value, you wont go it
+            if(nextButton.text.toString().length != 0){
+                return true;
+            }else{
+                oldButton.text="";
+                oldButton.setBackgroundColor(Color.parseColor("#ffffff"))
+
+                nextButton.text=character;
+                nextButton.setBackgroundColor(Color.parseColor("#E8A0BF"))
+
+                return false;
+            }
+        }
+    }
+    //end
+
     //start get stelected button and set Response text
     fun getSelectedButtonValue(index: Int){
 
@@ -170,8 +242,16 @@ class MainActivity : AppCompatActivity() {
 
     //start creat random character between of A and Z
     fun createRandomCharacter(): String {
-        val rand = ('A'..'Z').random()
+        val rand = ('A'..'Z').random();
         return rand.toString();
+    }
+    //end
+
+    //start create random column between of 0 and 7
+    fun createRandomColumn(): Int{
+        val rand = (0..7).random();
+
+        return rand;
     }
     //end
 
